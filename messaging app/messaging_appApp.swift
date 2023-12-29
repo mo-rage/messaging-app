7ebf89a958d8af7a77b8a0ae66e0d090a2a7dcf3
Mo Rage <ragemohamed32@gmail.com>
2023-12-29 15:24:19 -0800

    Initial Commit

---
+++ messaging app/messaging_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  messaging_appApp.swift
+//  messaging app
+//
+//  Created by Mohamed Rage on 2023-12-29.
+//
+
+import SwiftUI
+
+@main
+struct messaging_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
